19dd7fcdc96cfd9c5a43eb381218083906771952
Derek <derekraygao@gmail.com>
2019-10-31 23:24:00 -0700

    added in initial files

---
+++ src/main/java/starbucks/MyCards.java
@@ -0,0 +1,24 @@
+
+
+package starbucks ;
+
+/** My Cards Screen */
+public class MyCards extends Screen //inherits from screen.....
+{
+   
+    public MyCards()
+    {
+       
+    }
+
+    public String display() { //since it inherits from Screen, this overrides the display function?
+
+
+    	//string balance; //this should retrieve information from elsewhere
+
+    	//return "$" + balance;
+        return "hello2"; //should show balance here....
+
+    }
+   
+}
